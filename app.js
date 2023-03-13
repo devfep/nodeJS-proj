5d5404c7abcac79a9dc6669a5d951a58dfff684b
Felix P <devfep@gmail.com>
2023-03-13 01:47:01 -0400

    move error-handling logic to a file

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const notFound = require("./middleware/not-found");
+const errorHandler = require("./middleware/error-handler");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -32,14 +33,6 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.send(err);
-});
+app.use(errorHandler);
 
 module.exports = app;
