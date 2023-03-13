e8d6b11c186a3a769d022d064d58ab9e55def09d
Felix P <devfep@gmail.com>
2023-03-13 01:30:20 -0400

    add middleware for basic 404 error

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
+const notFound = require("./middleware/not-found");
+
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const tasksRouter = require("./routes/tasks");
@@ -20,6 +22,9 @@ app.use("/api/v1/tasks", tasksRouter);
 app.use("/", indexRouter);
 
 app.use("/users", usersRouter);
+
+//custom middleware for 404
+// app.use(notFound);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
