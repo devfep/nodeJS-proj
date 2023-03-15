d896a1578d8e9224245f49bcbcf654d5e96d0490
Felix P <devfep@gmail.com>
2023-03-14 23:55:43 -0400

    add http-errors npm pkg
    Use  http-errors npm pkg in place custom error middleware

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,6 +1,7 @@
 const { default: mongoose } = require("mongoose");
 const Task = require("../models/Task");
-const { createCustomError } = require("../middleware/errors/custom-errors");
+// const { createCustomError } = require("../middleware/errors/custom-errors");
+const createError = require("http-errors");
 const asyncWrapper = require("../utils/async");
 
 const getAllTasks = asyncWrapper(async (req, res, next) => {
@@ -18,8 +19,9 @@ const getTask = asyncWrapper(async (req, res, next) => {
   const task = await Task.findOne({ _id: taskID }).exec();
 
   if (!task) {
-    return next(createCustomError(`task with id ${taskID} not found`, 404));
+    // return next(createCustomError(`task with id ${taskID} not found`, 404));
     // return res.status(404).json({ msg: `task with id ${taskID} not found` });
+    return next(createError(404));
   }
   res.status(200).json(task);
 });
@@ -34,7 +36,7 @@ const updateTask = asyncWrapper(async (req, res, next) => {
     { new: true, runValidators: false }
   );
   if (!task) {
-    return next(createCustomError(`task with id ${taskID} not found`, 404));
+    return next(createError(404));
   }
   res.status(200).json({ id: taskID, data: req.body });
 });
@@ -43,7 +45,7 @@ const deleteTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndDelete({ _id: taskID });
   if (!task) {
-    return next(createCustomError(`task with id ${taskID} not found`, 404));
+    return next(createError(404));
   }
   res.status(200).json({ "status": "success", id: null });
 });
